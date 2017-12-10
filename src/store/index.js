2e879c79267fe8a136a883461201cf9e1a7a44e0
mluk <mailukasch@gmail.com>
2017-12-10 13:57:46 +0100

    added store

---
+++ src/store/index.js
@@ -0,0 +1,14 @@
+import { applyMiddleware, createStore } from 'redux';
+import { createLogger } from 'redux-logger';
+
+import rootReducer from '../reducers';
+
+const logger = createLogger({diff: true});
+
+const middlewares = [logger];
+
+const storeEnhancer = applyMiddleware(...middlewares);
+
+const store = createStore(rootReducer, storeEnhancer);
+
+export default store;
